f03b63e430a7227ae615ab076ee4e5c616e15ffa
Chrisjohnson89-L <afckazoo@gmail.com>
2014-07-10 11:51:05 -0700

    color slider working

--- js/app.js
+++ js/app.js
@@ -4,22 +4,32 @@ var color = $(".selected").css("background-color");
 
 //When clicking on control list items
 $(".controls li").click(function(){
-	//Deselect sibling elements
-	$(this).siblings().removeClass("selected");
-	//selct clicked element
-	$(this).addClass("selected");
-	//cache current color
-	color = $("this").css("background-color");
+  //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
+  //Select clicked element
+  $(this).addClass("selected");
+  //cache current color
+  color = $(this).css("background-color");
+});
+  
+//When "New Color" is pressed
+$("#revealColorSelect").click(function(){
+  //Show color select or hide the color select
+  changeColor();
+  $("#colorSelect").toggle();
 });
 
-//When new color is pressed
-$("#revealColorSelect").click(function(){
-	//show color select or hide the color select
-	$("#colorSelect").toggle();
-});
-	//show color select or hide the color select
+//update the new color span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
+}
 
 //When color sliders change
+$("input[type=range]").change(changeColor);
+
 	//update new color span
 
 //when add color is pressed
