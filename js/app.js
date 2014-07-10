d2852dcf4d7832eff7e2984f9b4c19596a574a8f
Chrisjohnson89-L <afckazoo@gmail.com>
2014-07-10 11:11:01 -0700

    Added visibility to color, Added color class selectors

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 //Probllem:No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
-
+var color = $(".selected").css("background-color");
 
 //When clicking on control list items
+$(".controls li").click(function(){
 	//Deselect sibling elements
+	$(this).siblings().removeClass("selected");
 	//selct clicked element
+	$(this).addClass("selected");
+	//cache current color
+	color = $("this").css("background-color");
+});
 
 //When new color is pressed
+$("#revealColorSelect").click(function(){
+	//show color select or hide the color select
+	$("#colorSelect").toggle();
+});
 	//show color select or hide the color select
 
 //When color sliders change
@@ -18,3 +28,4 @@
 
 //on mouse events on the canvas
 	//draw lines 
+
