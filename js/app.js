ffd978771050398ac0d5aafc429f0502040cc7d0
Chrisjohnson89-L <afckazoo@gmail.com>
2014-07-10 14:21:36 -0700

    final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-//Probllem:No user interaction causes no change to application
+//Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
-$(".controls li").click(function(){
+$(".controls").on("click", "li", function(){
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
@@ -30,12 +34,43 @@ function changeColor() {
 //When color sliders change
 $("input[type=range]").change(changeColor);
 
-	//update new color span
+//When "Add Color" is pressed
+$("#addNewColor").click(function(){
+  //Append the color to the controls ul
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
+  //Select the new color
+  $newColor.click();
+});
 
-//when add color is pressed
-	//append the color to the controls ul
-	//select the new color
+//On mouse events on the canvas
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
+  //Draw lines
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
 
-//on mouse events on the canvas
-	//draw lines 
+
+
+  
+
+
+
+
+
+
 
