766dcaa70dcddc67020b6cfe279c163c3bb40e58
Chrisjohnson89-L <afckazoo@gmail.com>
2014-07-10 10:09:24 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Probllem:No user interaction causes no change to application
+//Solution: When user interacts cause changes appropriately
+
+
+//When clicking on control list items
+	//Deselect sibling elements
+	//selct clicked element
+
+//When new color is pressed
+	//show color select or hide the color select
+
+//When color sliders change
+	//update new color span
+
+//when add color is pressed
+	//append the color to the controls ul
+	//select the new color
+
+//on mouse events on the canvas
+	//draw lines 
